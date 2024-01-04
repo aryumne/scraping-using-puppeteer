078b3f7af9a2cd746abd8efbd5c8c06130200443
Aryum N. Erliandi <erliandi.n.aryum@gmail.com>
2024-01-04 16:12:57 +0700

    fixing intercept api scraping module

--- modules/intercept-networks.js
+++ modules/intercept-networks.js
@@ -7,23 +7,43 @@ const interceptNetworks = async () => {
     executablePath: "/usr/bin/google-chrome-stable",
   });
   const page = await browser.newPage();
-  const apiUrlRegex = /^http:\/\/localhost:8000\/api\/v1\//;
+  // Define the regex pattern for the URL
+  const apiUrlRegex = /^https:\/\/ygtm.yamalubepromo.com\/api\/v1\//;
+  // Array to store response details
+  const responsesData = [];
 
+  // Listen for network responses
   page.on("response", async (response) => {
     const url = response.url();
+    // Check if the URL matches the regex pattern
     if (apiUrlRegex.test(url)) {
-      console.log("Request URL:", response.url());
-      console.log("Request Method:", response.request().method());
-      console.log("Response Status Code:", response.status());
       const resBody = await response.json();
-      console.log("Response Body:", resBody.data);
-      console.log("---");
+      const responseData = {
+        url: url,
+        method: response.request().method(),
+        statusCode: response.status(),
+        body: resBody.data,
+      };
+      // Add the response data to the array
+      responsesData.push(responseData);
     }
   });
-  await page.goto("http://localhost:5173/");
+  // Navigate to the desired page
+  await page.goto("https://ygtm.yamalubepromo.com/gallery");
 
-  console.log("Scraming done!");
-  //   await browser.close();
+  // Wait for all requests to be finished
+  await page.waitForNavigation({ waitUntil: "networkidle0" });
+
+  // Close the browser
+  await browser.close();
+  // Save responsesData to a JSON file
+
+  fs.writeFileSync(
+    "./exports/listApi.json",
+    JSON.stringify(responsesData, null, 2)
+  );
+
+  console.log("Scraping done!");
 };
 
 export default interceptNetworks;
